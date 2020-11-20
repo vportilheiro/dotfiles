9e4a671bf8efb2aebf39e2e89703b0dadd1c30a0
Vasco Portilheiro <vascop@vascos-mbp.lan>
2020-11-19 18:15:20 -0800

    Added installation script

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # dotfiles
 
 Each directory contains setup/dotfiles for the corresponding tool. Note: dotfiles are saved without changing names, so may be hidden.
-TODO: create an install script
+To install dotfiles, run
+```
+sh install.sh [tools]
+```
+where `tools` can be any of the directory names.
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# This script takes as arguments the names of tools/directories in the main
+# dotfiles directory. Each one of these tools is then installed under the
+# INSTALL_DIR directory (default to $HOME).
+
+# Exit immediately if any command returns with an error
+set -e
+
+# Main dotfiles project directory (location of this script)
+DOTFILES_DIR=$HOME/dotfiles
+
+# Directory to which dotfiles are symlinked
+#INSTALL_DIR=$HOME
+INSTALL_DIR=test
+
+# Extension added to existing files/directories that are replaced
+BACKUP_EXT=.pre-dotfiles-install
+
+# Takes the name of a tool/directory, and symlinks all hidden files to
+# INSTALL_DIR. Existsing files in INSTALL_DIR are backed up.
+symlink_hidden() {
+	for file in $DOTFILES_DIR/$1/.[!.]* $DOTFILES_DIR/$1/..?*; do
+		# Skip non-matched globs
+		[ -e $file ] || [ -L $file ] || continue
+		filename=`basename $file`
+		if [ -e $INSTALL_DIR/$filename ]; then
+			echo "Backing up $filename to $filename$BACKUP_EXT"
+			mv $INSTALL_DIR/$filename $INSTALL_DIR/$filename$BACKUP_EXT
+		fi
+		echo "Creating symlink at $INSTALL_DIR/$filename"
+		ln -s $file $INSTALL_DIR/$filename
+	done
+}
+
+# Symlinks .zshrc and .oh-my-zsh to INSTALL_DIR, and adds custom themes
+install_zsh() {
+	symlink_hidden zsh
+	ln -s $DOTFILES_DIR/zsh/*.zsh-theme $DOTFILES_DIR/zsh/.oh-my-zsh/custom/themes
+}
+
+main() {
+
+	# Make sure submodules are all installed and updated
+	git submodule update --init --recursive
+
+	# Parse arguments
+  while [ $# -gt 0 ]; do
+    case $1 in
+      bash) symlink_hidden bash;;
+			git) symlink_hidden git;;
+			tmux) symlink_hidden tmux;;
+      vim) symlink_hidden vim;;
+			zsh) install_zsh;;
+    esac
+    shift
+  done
+}
+
+main "$@"
